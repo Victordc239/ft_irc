88298aa0de6e54ab3e4e7f6c03239eaa3314c96d
Victor Diez <victordc239@gmail.com>
2026-03-02 17:43:07 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: victor <victor@student.42.fr>              +#+  +:+       +#+        */
+/*   By: vdiez-cu <vdiez-cu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/26 15:51:36 by vdiez-cu          #+#    #+#             */
-/*   Updated: 2026/03/02 11:19:20 by victor           ###   ########.fr       */
+/*   Updated: 2026/03/02 17:30:53 by vdiez-cu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,20 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
-#include <cstdlib>
 #include <fcntl.h>
 #include <poll.h>
+#include <cerrno>
+#include <cstdlib>
+#include <csignal>
+
+volatile sig_atomic_t g_running = 1;
+
+void sigint_handler(int signal)
+{
+    (void)signal; // ignorar parámetro
+    g_running = 0; // avisar al main loop que debe salir
+}
+
 
 /*esta funcion hace que el socket no sea blocante, es decir que el primer cliente
 bloquearia a los siguientes si no envia nada y los siguientes se quieren conectar o mandar algo*/
@@ -33,8 +44,32 @@ int set_nonblock(int fd)
 	return fcntl(fd, F_SETFL, flags | O_NONBLOCK);
 }
 
-int main()
+int main(int argc, char **argv)
 {
+	if (argc != 3)
+	{
+		std::cerr << "ERROR: error arguments\n";
+		return 1;
+	}
+
+	//manejar señales de interrupcion
+	signal(SIGINT, sigint_handler); //Ctrl+C
+	signal(SIGQUIT, sigint_handler); /*Ctrl+\ */ 
+	signal(SIGPIPE, SIG_IGN); // Escribir a socket cerrado
+
+	// parsear puerto
+	char *endptr = NULL;
+	long port = strtol(argv[1], &endptr, 10);
+	if (*endptr != '\0' || port <= 0 || port > 65535)
+	{
+		std::cerr << "Puerto inválido: " << argv[1] << "\n";
+		return 1;
+	}
+
+	// guardar contraseña
+	std::string server_password = argv[2];
+
+	
 	// Crear socket servidor que es el que escucha
 	int server_fd = socket(AF_INET, SOCK_STREAM, 0); //AF_INET=IPV4, SOCK_STREAM=TCP, 0=protocolo por defecto
 	if (server_fd == -1)
@@ -63,7 +98,7 @@ int main()
 	sockaddr_in server_addr; //estructura que ya existe para guardar ip y puerto del servidor
 	std::memset(&server_addr, 0, sizeof(server_addr)); //seteamos a 0 toda la estructura
 	server_addr.sin_family = AF_INET; //IPv4
-	server_addr.sin_port = htons(1201); //Conversion del numero del puerto en bits para que todos los ordenadores lo interpreten igual
+	server_addr.sin_port = htons((uint16_t)port); //Conversion del numero del puerto en bits para que todos los ordenadores lo interpreten igual
 	server_addr.sin_addr.s_addr = INADDR_ANY; //cualquier adapator de red del ordenador, ethernet, wifi, localhost,...
 
 	if (bind(server_fd, (sockaddr*)&server_addr, sizeof(server_addr)) == -1) //le decimos a que puerto y ip esta asociado el socket
@@ -89,20 +124,26 @@ int main()
 	// Estructura poll
 	std::vector<struct pollfd> fds;
 	pollfd pfd;
-	pfd.fd = server_fd;
-	pfd.events = POLLIN;
-	pfd.revents = 0;
+	pfd.fd = server_fd; //vigilar el servidor
+	pfd.events = POLLIN; //avisame cuando haya datos a leer
+	pfd.revents = 0; //eventos que han pasado
 	fds.push_back(pfd);
 
-	// Acumuladores por cliente
+	// Acumuladores por cliente para recibir todos los mensajes completos
 	std::map<int, std::string> accum;
 
+	//Buffer para enviar mensaje de servidor a cliente
+	std::map<int, std::string> outbuf;
+
+	//mapa para verificar si cada fd client a puesto correctamente la contraseña
+	std::map<int, bool> correctPass;
+
 	const size_t BUF_SIZE = 512;
-	char buf[BUF_SIZE];
+	char buf[BUF_SIZE]; //buffer para almacenar el mensaje que envias
 
 	while (true)
 	{
-		int ret = poll(&fds[0], fds.size(), -1); // espera indefinida
+		int ret = poll(&fds[0], fds.size(), -1); // espera que haya algun evento en un cliente o en el servidor
 		if (ret == -1)
 		{
 			std::cerr << "ERROR: poll falló\n";
@@ -115,29 +156,31 @@ int main()
 			if (fds[i].revents == 0)
 				continue;
 
-			// Error o desconexión
+			/*Error o desconexión en un cliente o en el servidor 
+			POLLERR=conexion rota, POLLHUP=cierras la terminal, POLLNVAL=fd corrupto*/
 			if (fds[i].revents & (POLLERR | POLLHUP | POLLNVAL))
 			{
 				if (fds[i].fd == server_fd)
 				{
-					std::cerr << "ERROR en server_fd (poll)\n";
 					// en caso crítico cerramos todo
+					std::cerr << "ERROR en server_fd (poll)\n";
 					close(server_fd);
 					return 1;
 				}
 				else
 				{
-					int badfd = fds[i].fd;
-					std::cout << "Cliente (fd " << badfd << ") se desconectó/err\n";
-					close(badfd);
-					accum.erase(badfd);
+					std::cout << "Cliente (fd " << fds[i].fd << ") se desconectó/err\n";
+					close(fds[i].fd);
+					accum.erase(fds[i].fd);
+					outbuf.erase(fds[i].fd);
+					correctPass.erase(fds[i].fd);
 					fds.erase(fds.begin() + i);
 					--i;
 					continue;
 				}
 			}
 
-			// Nueva conexión entrante
+			// Nueva conexión entrante, POLLIN=poll recibe datos a leer
 			if (fds[i].fd == server_fd && (fds[i].revents & POLLIN))
 			{
 				// aceptar tantas conexiones como estén pendientes
@@ -145,13 +188,16 @@ int main()
 				{
 					sockaddr_in client_addr;
 					socklen_t client_len = sizeof(client_addr);
-					int client_fd = accept(server_fd, (sockaddr*)&client_addr, &client_len);
+					int client_fd = accept(server_fd, (sockaddr*)&client_addr, &client_len); /*acepta y crea
+							un nuevo socket del nuevo cliente y devuelve el fd del nuevo cliente
+							(sockaddr*)&client_addr=la ip y el puerto del nuevo cliente*/
 					if (client_fd == -1)
 					{
-						// si errno == EWOULDBLOCK/EAGAIN no hay más conexiones pendientes
+						if (errno == EAGAIN || errno == EWOULDBLOCK)
+							break; // no hay más conexiones pendientes
+						std::cerr << "ERROR en accept nuevo cliente\n";
 						break;
 					}
-
 					// opcional: poner non-blocking al cliente
 					if (set_nonblock(client_fd) == -1)
 						std::cerr << "WARNING: no se pudo poner client_fd non-blocking\n";
@@ -163,8 +209,14 @@ int main()
 					cp.revents = 0;
 					fds.push_back(cp);
 
-					// inicializar acumulador
+					// inicializar acumulador para este nuevo cliente
 					accum[client_fd] = "";
+
+					// inicializar buffer salida del servidor para este nuevo cliente
+					outbuf[client_fd] = "";
+
+					// inicializar estado de autenticación (necesita PASS)
+					correctPass[client_fd] = false;
 
 					// imprimir IP del cliente
 					char client_ip[INET_ADDRSTRLEN];
@@ -180,55 +232,111 @@ int main()
 			// Datos desde un cliente existente
 			if (fds[i].revents & POLLIN)
 			{
-				int cfd = fds[i].fd;
-				ssize_t n = recv(cfd, buf, BUF_SIZE, 0);
-				if (n == -1)
+				int clientFd = fds[i].fd;
+				ssize_t n = recv(clientFd, buf, BUF_SIZE, 0); //la cantidad de bytes que ha enviado el cliente al servidor
+				if (n == -1) //ERROR al recibir los datos
 				{
-					// EWOULDBLOCK/EAGAIN puede suceder si non-blocking; ignoramos y seguimos
-					std::cerr << "ERROR: recv falló en fd " << cfd << "\n";
+					std::cerr << "ERROR: recv falló en fd " << clientFd << "\n";
 					// cerramos y limpiamos
-					close(cfd);
-					accum.erase(cfd);
+					close(clientFd);
+					accum.erase(clientFd);
+					outbuf.erase(clientFd);
 					fds.erase(fds.begin() + i);
 					--i;
 					continue;
 				}
-				if (n == 0)
+				if (n == 0)	// cliente cerró conexión
 				{
-					// cliente cerró conexión
-					std::cout << "Cliente (fd " << cfd << ") cerró conexión\n";
-					close(cfd);
-					accum.erase(cfd);
+					std::cout << "Cliente (fd " << clientFd << ") cerró conexión\n";
+					close(clientFd);
+					accum.erase(clientFd);
+					outbuf.erase(clientFd);
 					fds.erase(fds.begin() + i);
 					--i;
 					continue;
 				}
 
 				// añadir a acumulador
-				accum[cfd].append(buf, buf + n);
+				accum[clientFd].append(buf, buf + n);
 
 				// procesar líneas completas
 				size_t pos;
-				while ((pos = accum[cfd].find('\n')) != std::string::npos)
+				while ((pos = accum[clientFd].find('\n')) != std::string::npos)
 				{
-					std::string line = accum[cfd].substr(0, pos);
+					std::string line = accum[clientFd].substr(0, pos);
 					if (!line.empty() && line[line.size() - 1] == '\r')
 						line.erase(line.size() - 1);
 
 					// log en servidor
-					std::cout << "fd " << cfd << " -> Línea recibida: [" << line << "]\n";
+					std::cout << "fd " << clientFd << " -> Línea recibida: [" << line << "]\n";
 
-					// responder con CRLF
-					const char reply[] = "Servidor dice: hola\r\n";
-					ssize_t sent = send(cfd, reply, std::strlen(reply), 0);
-					if (sent == -1)
-						std::cerr << "ERROR: send falló en fd " << cfd << "\n";
+					if (!correctPass[clientFd])
+					{
+						// esperamos "PASS <password>"
+						if (line.compare(0, 5, "PASS ") == 0)
+						{
+							std::string given = line.substr(5);
+							if (given == server_password)
+							{
+								correctPass[clientFd] = true;
+								// confirmar (se encola para enviar correctamente con POLLOUT)
+								outbuf[clientFd] += "PASS accepted\r\n";
+								fds[i].events |= POLLOUT;
+								std::cout << "fd " << clientFd << " autenticado correctamente\n";
+							}
+							else
+							{
+								std::cout << "fd " << clientFd << " fallo autenticacion (PASS incorrecto). Cerrando.\n";
+								// cerramos sin mandar respuesta para no escribir sin poll
+								close(clientFd);
+								accum.erase(clientFd);
+								outbuf.erase(clientFd);
+								correctPass.erase(clientFd);
+								fds.erase(fds.begin() + i);
+								--i;
+								continue;
+							}
+						}
+						else
+						{
+							// no envió PASS primero => cerrar (el subject exige password)
+							std::cout << "fd " << clientFd << " no envió PASS primero. Cerrando.\n";
+							close(clientFd);
+							accum.erase(clientFd);
+							outbuf.erase(clientFd);
+							correctPass.erase(clientFd);
+							fds.erase(fds.begin() + i);
+							--i;
+							continue;
+						}
+					}
 					else
-						std::cout << "fd " << cfd << " <- Respuesta enviada (" << sent << " bytes)\n";
+					{
+						// comportamiento actual: preparar respuesta simple
+						outbuf[clientFd] += "Servidor dice: hola\r\n";
+						fds[i].events |= POLLOUT;
+					}
 
 					// borrar línea procesada (incluye '\n')
-					accum[cfd].erase(0, pos + 1);
+					accum[clientFd].erase(0, pos + 1);
 				}
+			}
+
+			// Enviar datos a cliente si está listo para escribir
+			if (fds[i].revents & POLLOUT)
+			{
+				int fd = fds[i].fd;
+				std::string &data = outbuf[fd];
+
+				if (!data.empty())
+				{
+					ssize_t sent = send(fd, data.c_str(), data.size(), 0);
+					if (sent > 0)
+						data.erase(0, sent);
+				}
+
+				if (data.empty())
+					fds[i].events &= ~POLLOUT;
 			}
 		} // fin for fds
 	} // fin while poll
